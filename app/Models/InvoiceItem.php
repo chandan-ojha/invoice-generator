c4d4fe526319eccabdde71e835f0f48aa4144cfd
Chandan Ojha <chandanojha776@gmail.com>
2023-09-15 19:31:39 +0600

    `invoice-table` created

---
+++ app/Models/InvoiceItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoiceItem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'invoice_items';
+    protected $guarded = [];
+}
---
+++ database/migrations/2023_09_15_131133_create_invoices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number');
+            $table->string('sender');
+            $table->string('bill_to');
+            $table->string('ship_to');
+            $table->date('date');
+            $table->date('due_date');
+            $table->string('additional_note');
+            $table->string('notes');
+            $table->string('terms');
+            $table->double('sub_total',8,2);
+            $table->integer('tax');
+            $table->double('total',8,2);
+            $table->bigInteger('invoice_item_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('invoice_item_id')->references('id')->on('invoice_items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
